97efdbc69a3b5ef2f4a23e533d1861d4dffa49e1
Jacek Janczura <janczura.jacek@gmail.com>
2020-01-11 17:07:00 +0100

    Fix the name of factory

--- test/Ownable.behavior.js
+++ test/Ownable.behavior.js
@@ -10,44 +10,55 @@ const { expect } = require("chai");
 function shouldBehaveLikeOwnable(owner, [other]) {
   describe("as an ownable", function() {
     it("should have an owner", async function() {
-      expect(await this.ownable.owner()).to.equal(owner);
+      expect(await this.factory.owner()).to.equal(owner);
     });
 
     it("changes owner after transfer", async function() {
-      expect(await this.ownable.isOwner({ from: other })).to.equal(false);
-      const receipt = await this.ownable.transferOwnership(other, {
-        from: owner
-      });
+      expect(
+        await this.factory.isOwner({ from: other })
+      ).to.equal(false);
+      const receipt = await this.factory.transferOwnership(
+        other,
+        {
+          from: owner
+        }
+      );
       expectEvent(receipt, "OwnershipTransferred");
 
-      expect(await this.ownable.owner()).to.equal(other);
-      expect(await this.ownable.isOwner({ from: other })).to.equal(true);
+      expect(await this.factory.owner()).to.equal(other);
+      expect(
+        await this.factory.isOwner({ from: other })
+      ).to.equal(true);
     });
 
     it("should prevent non-owners from transferring", async function() {
       await expectRevert(
-        this.ownable.transferOwnership(other, { from: other }),
+        this.factory.transferOwnership(other, { from: other }),
         "Ownable: caller is not the owner"
       );
     });
 
     it("should guard ownership against stuck state", async function() {
       await expectRevert(
-        this.ownable.transferOwnership(ZERO_ADDRESS, { from: owner }),
+        this.factory.transferOwnership(ZERO_ADDRESS, {
+          from: owner
+        }),
         "Ownable: new owner is the zero address"
       );
     });
 
     it("loses owner after renouncement", async function() {
-      const receipt = await this.ownable.renounceOwnership({ from: owner });
+      const receipt = await this.factory.renounceOwnership({
+        from: owner
+      });
       expectEvent(receipt, "OwnershipTransferred");
 
-      expect(await this.ownable.owner()).to.equal(ZERO_ADDRESS);
+      expect(await this.factory.owner()).to.equal(ZERO_ADDRESS);
     });
 
     it("should prevent non-owners from renouncement", async function() {
       await expectRevert(
-        this.ownable.renounceOwnership({ from: other }),
+        this.factory.renounceOwnership({ from: other }),
         "Ownable: caller is not the owner"
       );
     });
